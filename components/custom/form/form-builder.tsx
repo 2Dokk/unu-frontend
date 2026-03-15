bcc103c3cf2f48bf5d51fcb53f4a8269f155a6fb
tjdus <dusml0277@gmail.com>
2026-03-15 12:59:17 +0900

    fix: question grid

--- components/custom/form/form-builder.tsx
+++ components/custom/form/form-builder.tsx
@@ -110,7 +110,7 @@ export function FormBuilder({ initialSchema, onChange }: FormBuilderProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+    <div className="grid grid-cols-1 lg:grid-cols-[3fr_2fr] gap-6">
       {/* Left: Edit Section */}
       <Card>
         <CardHeader>
